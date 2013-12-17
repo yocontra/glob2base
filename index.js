3b8bb7084ac84578417ee762cdd299a47ab1609a
Contra <contra@maricopa.edu>
2013-12-16 20:20:22 -0700

    first one

---
+++ index.js
@@ -0,0 +1,18 @@
+var path = require('path');
+
+var flatten2d = function(arr){
+  return arr.map(function(s, idx) {
+    if (typeof s === 'string' && idx !== arr.length-1) {
+      return s;
+    } else {
+      return '';
+    }
+  });
+};
+
+module.exports = function(glob) {
+  var cwd = (glob.options && glob.options.cwd) ? glob.options.cwd : process.cwd();
+  var rules = glob.minimatch.set[0];
+  var basePath = path.normalize(flatten2d(rules).join(path.sep));
+  return path.join(cwd, basePath);
+};
---
+++ test/main.js
@@ -0,0 +1,13 @@
+var glob2base = require('../');
+var glob = require('glob');
+var path = require('path');
+var should = require('should');
+require('mocha');
+
+describe('glob2base', function() {
+  it('should get a base name', function(done) {
+    var globber = new glob.Glob("js/*.js", {cwd: __dirname});
+    glob2base(globber).should.equal(path.join(__dirname, "js/"));
+    done();
+  });
+});
